d19c62b47c5aa32ab984d3245f6f1701624bd48d
Justin Licata <licata.justin@gmail.com>
2016-12-12 11:42:52 -0600

    initial commit :sunrise_over_mountains:

---
+++ README.md
@@ -0,0 +1,30 @@
+# Hyper Sierra Vibrancy
+
+A dark theme with vibrancy for Hyper inspired by the Sierra Nevada.
+
+---
+
+![Screenshot](https://raw.githubusercontent.com/licatajustin/hyper-sierra-vibrancy/master/images/screenshot.png)
+
+### Installation
+
+1. Open Hyper's preferences with `Cmd + ,` (or manually at `~/.hyper.js`).
+2. Update your list of plugins to include `hyper-sierra-vibrancy`, like this:
+
+  ```javascript
+  plugins: [
+    'hyper-sierra-vibrancy'
+  ]
+  ```
+3. Fully reload Hyper (`Cmd + Shift + R`)
+4. Profit!
+
+### Related
+
+- [Hyper](https://hyper.is/)
+- [Hyper Sierra](https://github.com/licatajustin/hyper-sierra)
+- [Sierra Syntax for Atom](https://github.com/licatajustin/sierra-syntax)
+
+### License
+
+MIT @ [Justin Licata](https://twitter.com/justinlicata)
---
+++ index.js
@@ -0,0 +1,70 @@
+// Constants
+const backgroundColor = 'rgba(4, 9, 15, 0.75)';
+const foregroundColor = '#cbcfd4';
+
+// Colors
+const RED = '#de4f55';
+const GREEN = '#38dec9';
+const YELLOW = '#f0c981';
+const BLUE = '#81629e';
+const MAGENTA = '#dbbfe3';
+const LIGHT_GRAY = '#f2f2f2';
+const DARK_GRAY = '#596a86'
+
+// Mapped Colors
+const colors = {
+  black: backgroundColor,
+  red: RED,
+  green: GREEN,
+  yellow: YELLOW,
+  blue: BLUE,
+  magenta: MAGENTA,
+  cyan: BLUE,
+  white: LIGHT_GRAY,
+  lightBlack: DARK_GRAY,
+  lightRed: RED,
+  lightGreen: GREEN,
+  lightYellow: YELLOW,
+  lightBlue: BLUE,
+  lightMagenta: MAGENTA,
+  lightCyan: BLUE,
+  colorCubes: '#fff',
+  grayscale: foregroundColor
+};
+
+// Additional Constants
+const cursorColor = YELLOW;
+const borderColor = backgroundColor;
+
+exports.onWindow = browserWindow => browserWindow.setVibrancy('dark');
+exports.decorateConfig = (config) => {
+  return Object.assign({}, config, {
+    foregroundColor,
+    backgroundColor,
+    borderColor,
+    cursorColor,
+    colors,
+    css: `
+      ${config.css || ''}
+      .cursor-node {
+        background-color: ${MAGENTA} !important;
+        border-color: ${MAGENTA} !important;
+      }
+      .hyper_main {
+        border: none !important;
+      }
+      .header_header {
+        background: rgba(4, 9, 15, 0.1) !important;
+      }
+      .splitpane_divider {
+        background-colo4, 9, 15, 0.35) !important;
+      }
+      .tab_tab {
+        border: 0;
+      }
+      .tab_textActive {
+        border-bottom: 2px solid ${BLUE};
+      }
+    `
+  })
+}
